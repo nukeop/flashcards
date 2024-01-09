d7a66c332c0d1c86109188eb97aaf8000cdb0fc7
nukeop <12746779+nukeop@users.noreply.github.com>
2024-01-09 22:51:31 +0100

    Lint database

--- src/app/_lib/database.types.ts
+++ src/app/_lib/database.types.ts
@@ -1,211 +1,217 @@
 export type Json =
-  | string
-  | number
-  | boolean
-  | null
-  | { [key: string]: Json | undefined }
-  | Json[]
+    | string
+    | number
+    | boolean
+    | null
+    | { [key: string]: Json | undefined }
+    | Json[];
 
 export interface Database {
-  public: {
-    Tables: {
-      decks: {
-        Row: {
-          created_at: string
-          description: string
-          id: string
-          name: string
-          public: boolean
-          updated_at: string
-          user_id: string
-        }
-        Insert: {
-          created_at?: string
-          description: string
-          id: string
-          name: string
-          public?: boolean
-          updated_at?: string
-          user_id: string
-        }
-        Update: {
-          created_at?: string
-          description?: string
-          id?: string
-          name?: string
-          public?: boolean
-          updated_at?: string
-          user_id?: string
-        }
-        Relationships: [
-          {
-            foreignKeyName: "decks_user_id_fkey"
-            columns: ["user_id"]
-            isOneToOne: false
-            referencedRelation: "users"
-            referencedColumns: ["id"]
-          }
-        ]
-      }
-      flashcards: {
-        Row: {
-          back: string
-          created_at: string
-          deck_id: string
-          front: string
-          id: string
-          updated_at: string
-        }
-        Insert: {
-          back: string
-          created_at?: string
-          deck_id: string
-          front: string
-          id: string
-          updated_at?: string
-        }
-        Update: {
-          back?: string
-          created_at?: string
-          deck_id?: string
-          front?: string
-          id?: string
-          updated_at?: string
-        }
-        Relationships: [
-          {
-            foreignKeyName: "flashcards_deck_id_fkey"
-            columns: ["deck_id"]
-            isOneToOne: false
-            referencedRelation: "decks"
-            referencedColumns: ["id"]
-          }
-        ]
-      }
-      user_profiles: {
-        Row: {
-          created_at: string
-          id: string
-          updated_at: string
-          user_id: string
-          username: string
-        }
-        Insert: {
-          created_at?: string
-          id: string
-          updated_at?: string
-          user_id: string
-          username: string
-        }
-        Update: {
-          created_at?: string
-          id?: string
-          updated_at?: string
-          user_id?: string
-          username?: string
-        }
-        Relationships: [
-          {
-            foreignKeyName: "user_profiles_user_id_fkey"
-            columns: ["user_id"]
-            isOneToOne: false
-            referencedRelation: "users"
-            referencedColumns: ["id"]
-          }
-        ]
-      }
-    }
-    Views: {
-      [_ in never]: never
-    }
-    Functions: {
-      [_ in never]: never
-    }
-    Enums: {
-      [_ in never]: never
-    }
-    CompositeTypes: {
-      [_ in never]: never
-    }
-  }
+    public: {
+        Tables: {
+            decks: {
+                Row: {
+                    created_at: string;
+                    description: string;
+                    id: string;
+                    name: string;
+                    public: boolean;
+                    updated_at: string;
+                    user_id: string;
+                };
+                Insert: {
+                    created_at?: string;
+                    description: string;
+                    id: string;
+                    name: string;
+                    public?: boolean;
+                    updated_at?: string;
+                    user_id: string;
+                };
+                Update: {
+                    created_at?: string;
+                    description?: string;
+                    id?: string;
+                    name?: string;
+                    public?: boolean;
+                    updated_at?: string;
+                    user_id?: string;
+                };
+                Relationships: [
+                    {
+                        foreignKeyName: 'decks_user_id_fkey';
+                        columns: ['user_id'];
+                        isOneToOne: false;
+                        referencedRelation: 'users';
+                        referencedColumns: ['id'];
+                    },
+                ];
+            };
+            flashcards: {
+                Row: {
+                    back: string;
+                    created_at: string;
+                    deck_id: string;
+                    front: string;
+                    id: string;
+                    updated_at: string;
+                };
+                Insert: {
+                    back: string;
+                    created_at?: string;
+                    deck_id: string;
+                    front: string;
+                    id: string;
+                    updated_at?: string;
+                };
+                Update: {
+                    back?: string;
+                    created_at?: string;
+                    deck_id?: string;
+                    front?: string;
+                    id?: string;
+                    updated_at?: string;
+                };
+                Relationships: [
+                    {
+                        foreignKeyName: 'flashcards_deck_id_fkey';
+                        columns: ['deck_id'];
+                        isOneToOne: false;
+                        referencedRelation: 'decks';
+                        referencedColumns: ['id'];
+                    },
+                ];
+            };
+            user_profiles: {
+                Row: {
+                    created_at: string;
+                    id: string;
+                    updated_at: string;
+                    user_id: string;
+                    username: string;
+                };
+                Insert: {
+                    created_at?: string;
+                    id: string;
+                    updated_at?: string;
+                    user_id: string;
+                    username: string;
+                };
+                Update: {
+                    created_at?: string;
+                    id?: string;
+                    updated_at?: string;
+                    user_id?: string;
+                    username?: string;
+                };
+                Relationships: [
+                    {
+                        foreignKeyName: 'user_profiles_user_id_fkey';
+                        columns: ['user_id'];
+                        isOneToOne: false;
+                        referencedRelation: 'users';
+                        referencedColumns: ['id'];
+                    },
+                ];
+            };
+        };
+        Views: {
+            [_ in never]: never;
+        };
+        Functions: {
+            [_ in never]: never;
+        };
+        Enums: {
+            [_ in never]: never;
+        };
+        CompositeTypes: {
+            [_ in never]: never;
+        };
+    };
 }
 
 export type Tables<
-  PublicTableNameOrOptions extends
-    | keyof (Database["public"]["Tables"] & Database["public"]["Views"])
-    | { schema: keyof Database },
-  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
-        Database[PublicTableNameOrOptions["schema"]]["Views"])
-    : never = never
+    PublicTableNameOrOptions extends
+        | keyof (Database['public']['Tables'] & Database['public']['Views'])
+        | { schema: keyof Database },
+    TableName extends PublicTableNameOrOptions extends {
+        schema: keyof Database;
+    }
+        ? keyof (Database[PublicTableNameOrOptions['schema']]['Tables'] &
+              Database[PublicTableNameOrOptions['schema']]['Views'])
+        : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
-      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R
-    }
-    ? R
-    : never
-  : PublicTableNameOrOptions extends keyof (Database["public"]["Tables"] &
-      Database["public"]["Views"])
-  ? (Database["public"]["Tables"] &
-      Database["public"]["Views"])[PublicTableNameOrOptions] extends {
-      Row: infer R
-    }
-    ? R
-    : never
-  : never
+    ? (Database[PublicTableNameOrOptions['schema']]['Tables'] &
+          Database[PublicTableNameOrOptions['schema']]['Views'])[TableName] extends {
+          Row: infer R;
+      }
+        ? R
+        : never
+    : PublicTableNameOrOptions extends keyof (Database['public']['Tables'] &
+            Database['public']['Views'])
+      ? (Database['public']['Tables'] &
+            Database['public']['Views'])[PublicTableNameOrOptions] extends {
+            Row: infer R;
+        }
+          ? R
+          : never
+      : never;
 
 export type TablesInsert<
-  PublicTableNameOrOptions extends
-    | keyof Database["public"]["Tables"]
-    | { schema: keyof Database },
-  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never
+    PublicTableNameOrOptions extends
+        | keyof Database['public']['Tables']
+        | { schema: keyof Database },
+    TableName extends PublicTableNameOrOptions extends {
+        schema: keyof Database;
+    }
+        ? keyof Database[PublicTableNameOrOptions['schema']]['Tables']
+        : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I
-    }
-    ? I
-    : never
-  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
-  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
-      Insert: infer I
-    }
-    ? I
-    : never
-  : never
+    ? Database[PublicTableNameOrOptions['schema']]['Tables'][TableName] extends {
+          Insert: infer I;
+      }
+        ? I
+        : never
+    : PublicTableNameOrOptions extends keyof Database['public']['Tables']
+      ? Database['public']['Tables'][PublicTableNameOrOptions] extends {
+            Insert: infer I;
+        }
+          ? I
+          : never
+      : never;
 
 export type TablesUpdate<
-  PublicTableNameOrOptions extends
-    | keyof Database["public"]["Tables"]
-    | { schema: keyof Database },
-  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never
+    PublicTableNameOrOptions extends
+        | keyof Database['public']['Tables']
+        | { schema: keyof Database },
+    TableName extends PublicTableNameOrOptions extends {
+        schema: keyof Database;
+    }
+        ? keyof Database[PublicTableNameOrOptions['schema']]['Tables']
+        : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U
-    }
-    ? U
-    : never
-  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
-  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
-      Update: infer U
-    }
-    ? U
-    : never
-  : never
+    ? Database[PublicTableNameOrOptions['schema']]['Tables'][TableName] extends {
+          Update: infer U;
+      }
+        ? U
+        : never
+    : PublicTableNameOrOptions extends keyof Database['public']['Tables']
+      ? Database['public']['Tables'][PublicTableNameOrOptions] extends {
+            Update: infer U;
+        }
+          ? U
+          : never
+      : never;
 
 export type Enums<
-  PublicEnumNameOrOptions extends
-    | keyof Database["public"]["Enums"]
-    | { schema: keyof Database },
-  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
-    : never = never
+    PublicEnumNameOrOptions extends
+        | keyof Database['public']['Enums']
+        | { schema: keyof Database },
+    EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+        ? keyof Database[PublicEnumNameOrOptions['schema']]['Enums']
+        : never = never,
 > = PublicEnumNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
-  : PublicEnumNameOrOptions extends keyof Database["public"]["Enums"]
-  ? Database["public"]["Enums"][PublicEnumNameOrOptions]
-  : never
+    ? Database[PublicEnumNameOrOptions['schema']]['Enums'][EnumName]
+    : PublicEnumNameOrOptions extends keyof Database['public']['Enums']
+      ? Database['public']['Enums'][PublicEnumNameOrOptions]
+      : never;
