20601fb7bf38a6f387d6cd2e9ddc4fd82d3803d1
nukeop <12746779+nukeop@users.noreply.github.com>
2024-01-31 23:33:22 +0100

    Add e2e folder to test exclude list

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
     plugins: [react()],
     test: {
         environment: 'jsdom',
-        exclude: ['**/node_modules/**', './supabase/**/*'],
+        exclude: ['**/node_modules/**', './supabase/**/*', './e2e/**/*'],
     },
 });
