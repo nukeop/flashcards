94e5de3f70aebdbb31c52891b22cee66693c4b00
nukeop <12746779+nukeop@users.noreply.github.com>
2024-06-04 00:21:49 +0200

    DeckBuilder test util

--- supabase/test-utils.ts
+++ supabase/test-utils.ts
@@ -52,3 +52,53 @@ export class TestFixture {
         await TestFixture.supabase.auth.signOut();
     }
 }
+
+export class DeckBuilder {
+    private id: string;
+    private name: string;
+    private description: string;
+    private is_public: boolean;
+    private user_id: string;
+    private created_at: string;
+    private _modified: string;
+    private _deleted: boolean;
+
+    constructor() {
+        this.id = v4();
+        this.name = 'Deck';
+        this.description = 'Description for deck';
+        this.is_public = true;
+        this.user_id = '';
+        this.created_at = '';
+        this._modified = '';
+        this._deleted = false;
+    }
+
+    public withName(name: string) {
+        this.name = name;
+        return this;
+    }
+
+    public withDescription(description: string) {
+        this.description = description;
+        return this;
+    }
+
+    public async withUserId(client: SupabaseClient) {
+        this.user_id = (await client.auth.getUser()).data?.user?.id ?? '';
+        return this;
+    }
+
+    build(): Deck {
+        return {
+            id: this.id,
+            name: this.name,
+            description: this.description,
+            is_public: this.is_public,
+            user_id: this.user_id,
+            created_at: this.created_at,
+            _modified: this._modified,
+            _deleted: this._deleted,
+        };
+    }
+}
